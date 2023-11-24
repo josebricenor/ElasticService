7e2b6c8adced771cc668f34a41830139156e1fd6
josebricenor <josebricenor@gmail.com>
2023-11-24 14:52:06 -0500

    Readme update

--- terraform/deployment/deployment.tf
+++ terraform/deployment/deployment.tf
@@ -13,33 +13,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-/*resource "aws_security_group" "elastic_beanstalk_sg" {
-  name        = "elastic-beanstalk-sg"
-  description = "Security group for Elastic Beanstalk environment"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}*/
-
 resource "aws_elastic_beanstalk_application" "elastic_service_app" {
   name        = "ElasticServiceApplication"
   description = "ElasticService Application"
@@ -58,8 +31,6 @@ resource "aws_elastic_beanstalk_environment" "elastic_service_env" {
   application         = aws_elastic_beanstalk_application.elastic_service_app.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.1.1 running Corretto 17"
   version_label       = aws_elastic_beanstalk_application_version.elastic_service_version.name
-
-  //depends_on = [aws_security_group.elastic_beanstalk_sg]
 
   setting {
     namespace = "aws:autoscaling:asg"
